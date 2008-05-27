dc18fee20d001f9dc2b5625ea9235537b77313f6
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-05-27 15:05:59 +0000

    Initial import

---
+++ trunk/smarty/templates_c/%%F7^F7F^F7F34188%%header.tpl.php
@@ -0,0 +1,13 @@
+<?php /* Smarty version 2.6.19, created on 2008-05-27 10:32:07
+         compiled from header.tpl */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Smarty</title>
+	
+</head>
+<body>
