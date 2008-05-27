03fd4f6d7c91c057bb70a1963d8161751f0318a6
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-05-27 15:13:20 +0000

    Initial import

---
+++ trunk/ya/smarty/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.19, created on 2008-05-27 13:43:31
+         compiled from footer.tpl */ ?>
+<p><a href="http://www.validome.org/referer">valid xhtm 1.0</a></p>
+</body>
+</html>
