5b483c9369e8d0b689a54fcc2cfe9f26f1ce03b9
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-09 13:29:30 +0000

    added compile dir

---
+++ compile/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.19, created on 2008-06-09 15:28:46
+         compiled from footer.tpl */ ?>
+<p><a href="http://www.validome.org/referer">valid xhtm 1.0</a></p>
+</body>
+</html>
