d730b99c4f58d18b39d0d8b30f2208af90f69efd
ccflyer <ccflyer@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-09 08:05:19 +0000

    changed small things

--- html/register.php
+++ html/register.php
@@ -15,6 +15,43 @@ while (($row = $res->fetchRow())) {
 }
 
 $t->assign('stateOptions', $row_array);
+
+if(isset($_GET['e']))
+{
+	$errorcode = $_GET['e'];
+	
+	switch($errorcode)
+	{
+		case 1:
+			$t->assign('errormessage', 'Your username is not unique in the database');
+			break;
+		case 2:
+			$t->assign('errormessage', 'Passwords must match!');
+			break;
+		case 3:
+			$t->assign('errormessage', 'Username is invalid');
+			break;
+		case 4:
+			$t->assign('errormessage', 'Invalid address, please enter a correct Address');
+			break;
+		case 5:
+			$t->assign('errormessage', 'Invalid city, please enter a correct city');
+			break;
+		case 6:
+			$t->assign('errormessage', 'Invalid state, please enter a correct state');
+			break;
+		case 7:
+			$t->assign('errormessage', 'Please enter a valid zip code');
+			break;
+		case 8:
+			$t->assign('errormessage', 'Incorrect Name - only alphabetic characters allowed');
+			break;
+		case 9:
+			$t->assign('errormessage', 'Please enter a valid email address');
+			break;
+	}
+}
+
 //print_r($row_array);
 $t->display('register.tpl');
 ?>
