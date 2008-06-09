f9cbd24ee00eba6c4d2eabf5462a65e71dfdd9c9
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-09 13:27:24 +0000

    svnignore?

--- trunk/compile/%%17^171^171FECE9%%topbar.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-06-09 14:45:40
-         compiled from topbar.tpl */ ?>
-<p><b>top bar</b></p>
--- trunk/compile/%%F8^F8A^F8AC46EE%%page1.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-06-05 18:26:55
-         compiled from page1.tpl */ ?>
-<p><b>html content here</b></p>
