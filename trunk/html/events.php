53ac286a4976b35d37e3be77b275da5dcf588a7b
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-10 08:44:14 +0000

    example of using executeQuery with parameters

--- trunk/html/events.php
+++ trunk/html/events.php
@@ -5,7 +5,8 @@ require_once('handleQuery.php');
 
 //$myarray=executeQuery('select * from view_event_info');
 $values[]=1;
-$myarray=executeQuery('select * from view_event_info where event_id = ?',$values);
+$values[]='2008-06-09';
+$myarray=executeQuery('select * from view_event_info where event_id = ? and date > ? order by date asc',$values);
 $t->assign('data',$myarray);
 $t->display("events.tpl");
 
