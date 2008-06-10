84671a6449fc56a04dcfe743fb1e0ada4d13f2d3
ccflyer <ccflyer@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-10 16:19:42 +0000

    Added basic cart

---
+++ html/cart.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('set_env.php');
+require_once('handleQuery.php');
+require('basket_fns.php');
+
+$values[] = array();
+$values['id'] = '1';
+$myarray=executeQuery('SELECT events.name, events.date, ticket_type.price, basket.number_of_tickets, basket.id FROM events, ticket_type, basket WHERE basket.event_id = events.id AND ticket_type.id = basket.ticket_type_id AND basket.user_id = ?;',$values['id']);
+
+$t->assign('data',$myarray);
+
+$t->display('cart.tpl');
+
+?>
