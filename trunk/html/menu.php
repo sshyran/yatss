9ddd34fb9bdb5a2ff659b64e137704aa27f1f6fa
ccflyer <ccflyer@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-12 08:08:20 +0000

    changed slight things in tickets

--- trunk/html/menu.php
+++ trunk/html/menu.php
@@ -1,7 +1,7 @@
 <?php
 //require_once('set_env.php');
 
-$valid_pages=array('events', 'history','cart', 'register', 'users', 'my_account');
+$valid_pages=array('events', 'history','cart', 'register', 'users', 'my_account', 'tickets');
 $valid_pages_registered=array('events', 'history','cart', 'my_account');
 $valid_pages_browser=array('events', 'register');
 $valid_pages_admin=array('events', 'history', 'users');
