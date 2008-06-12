73c795a24c667d467fa2044ace23643392c5959e
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-12 07:46:24 +0000

    link to tickets

--- html/events.php
+++ html/events.php
@@ -7,9 +7,9 @@ $values[]= $db->quote(date($_date_format));
 $myarray=executeQuery('select event_id, name, description from view_event_info where date > ? order by date asc',$values);
 
 foreach ($myarray as $key => &$value) {
-	if ($a->checkAuth()) {
-		$value['link']="<a href=\"$web_root?page=cart&amp;event_id=$value[event_id]&amp;number_of_tickets=1\">display details</a>";
-	}
+//	if ($a->checkAuth()) {
+		$value['link']="<a href=\"$web_root?page=tickets&amp;event_id=$value[event_id]\">details</a>";
+//	}
 	array_shift($value);
 	unset($value['ticket_type_id']);
 }
