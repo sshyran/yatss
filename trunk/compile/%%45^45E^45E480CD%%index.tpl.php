15bfc46daa43ba4b26ab66c935453aedbe2db520
ccflyer <ccflyer@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-12 09:05:00 +0000

    testing svn ignore

--- trunk/compile/%%45^45E^45E480CD%%index.tpl.php
+++ trunk/compile/%%45^45E^45E480CD%%index.tpl.php
@@ -1,13 +1,11 @@
-<?php /* Smarty version 2.6.19, created on 2008-06-09 15:28:43
+<?php /* Smarty version 2.6.19, created on 2008-06-12 10:35:28
          compiled from index.tpl */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => ($this->_tpl_vars['tpl_name']).".tpl", 'smarty_include_vars' => array()));
+<?php $_from = $this->_tpl_vars['tplList']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+    foreach ($_from as $this->_tpl_vars['i']):
+?>
+	<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => ($this->_tpl_vars['i']).".tpl", 'smarty_include_vars' => array()));
 $this->_tpl_vars = $_smarty_tpl_vars;
 unset($_smarty_tpl_vars);
  ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => ($this->_tpl_vars['tpl_name_login']).".tpl", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
-<div>main page</div>
+<?php endforeach; endif; unset($_from); ?>
