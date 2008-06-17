f5b10edd952b101fcb9589f812405569bbfc44f0
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-17 16:16:04 +0000

    wtf?

--- compile/%%17^171^171FECE9%%topbar.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-06-17 17:11:01
-         compiled from topbar.tpl */ ?>
-<div class="wrap">
-<h1><a href="<?php echo $this->_tpl_vars['web_root']; ?>
-">Project YATSS</a></h1>
