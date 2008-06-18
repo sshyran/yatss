a842577e3ddb5ec8f2d8949d08093441cf6c13a8
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-18 15:21:44 +0000

    navigation on event page improved

--- trunk/html/events.php
+++ trunk/html/events.php
@@ -4,16 +4,15 @@ require_once('util.php');
 
 function events($page_number=0)
 {
-	$step=7;
+	$step=5;
 	global $t,$web_root;
 	$count_of_rows=executeQuery('select count(distinct event_id) as num from view_event_info where date > CURRENT_TIMESTAMP');
 	$count_of_rows=$count_of_rows[0]['num'];
 	$limitfrom=$page_number*$step;
-	$limitto=$limitfrom+$step;
 	if ($limitfrom>=$count_of_rows) {
-		$limitfrom=0; $limitto=$step; $page_number=-1;
+		$limitfrom=0; $page_number=-1;
 	}
-	$myarray=executeQuery('select event_id, name, date_format(date, "%Y-%m-%d %H:%i") as "event date", description from view_event_info where date > CURRENT_TIMESTAMP group by event_id order by date asc limit '.$limitfrom .', '. $limitto);
+	$myarray=executeQuery('select event_id, name, date_format(date, "%Y-%m-%d %H:%i") as "event date", description from view_event_info where date > CURRENT_TIMESTAMP group by event_id order by date asc limit '.$limitfrom .', '. $step);
 
 	foreach ($myarray as $key => &$value) {
 		$value['link']="<a href=\"$web_root?page=tickets&amp;event_id=$value[event_id]\">details</a>";
@@ -22,7 +21,12 @@ function events($page_number=0)
 	}
 	$t->assign('display_first_row',1);
 	$t->assign('data',$myarray);
-	$t->assign('pagenum',$page_number+1);
+	if (round($count_of_rows / $step)>$page_number) {
+		$t->assign('next_page',$page_number+1);		
+	}
+	if ($page_number) {
+		$t->assign('prev_page',$page_number-1);
+	}
 }
 
 if (isset($_GET['page_number']) && is_numeric($_GET['page_number'])) {
--- trunk/tpl/events.tpl
+++ trunk/tpl/events.tpl
@@ -1,13 +1,11 @@
 <div id="left">
 {include file='showGrid.tpl'}
 </div>
-{*
-<span id="buttons" style="float:right;position:relative;clear:both;bottom: 30px;right:90px">
-	{if $pagenum >1}
-		<a href="{$web_root}?page=events&page_number={$pagenum-2}">previous page</a>
-		</span>
-	{/if}
-*}
 <span id="buttons" style="float: right;position: relative;clear: both;bottom: 30px;">
-<a href="{$web_root}?page=events&page_number={$pagenum|default:'0'}">next page</a>
+	{if isset($prev_page)}
+		<a href="{$web_root}?page=events&amp;page_number={$prev_page}">previous page</a>
+	{/if}
+	{if isset($next_page)}
+		<a href="{$web_root}?page=events&amp;page_number={$next_page}">next page</a>
+	{/if}
 </span>
