e2c34e3b63207297594861155bf0da103d498d80
rollxx <rollxx@759054a9-344e-0410-8af0-c158bd36ed70>
2008-06-18 06:57:30 +0000

    renamed files to get rid of underlines in menu

---
+++ html/account.php
@@ -0,0 +1,16 @@
+<?php
+require_once('set_env.php');
+require_once('util.php');
+
+if ($a->checkAuth()) {
+	$values[]=$_SESSION['userid'];
+	$sql='select username as nickname, address, city, us_states.name as state, zip, firstname as "first name", middlename as "middle name", lastname as "last name", email from users, address, us_states where users.id= ? and address.id=users.address_id and address.state_id=us_states.id';
+	$myarray=executeQuery($sql,$values);
+	$t->assign('display_first_row',1);
+	$t->assign('data',$myarray);
+}
+else {
+	header("location:$web_root?page=message_page&message_id=52");
+	exit;
+}
+?>
